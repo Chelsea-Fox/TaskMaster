6525af96115f28e8f0d1af8e66c2e77429931843
kjhohura244 <kjhohura@yahoo.co.uk>
2023-06-16 12:15:02 +0100

    Add invalid task tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,8 @@ Main test file for TaskMaster
 """
 from datetime import datetime, timedelta
 from unittest import TestCase
+import pytest
+from schema import SchemaMissingKeyError
 from main import get_task
 
 
@@ -26,3 +28,23 @@ class Test(TestCase):
         return_value = get_task(self.clean_task)
 
         assert return_value is not None
+
+    def test_invalid_task_throws_exception(self):
+        """Invalid of task"""
+        invalid_task = {
+            "descriptionzzzzzzzzzzzzzz": "Cooking",
+            "eta": datetime.now() + timedelta(days=3),
+            "status": "OPEN",
+        }
+        with pytest.raises(SchemaMissingKeyError):
+            get_task(invalid_task)
+
+    def test_invalid_status(self):
+        """Invalid task status"""
+        invalid_status_task = {
+            "description": "Cooking",
+            "eta": datetime.now() + timedelta(days=3),
+            "status": "BLUE",
+        }
+        with pytest.raises(ValueError):
+            get_task(invalid_status_task)
