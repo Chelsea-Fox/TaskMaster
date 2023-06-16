f9e379f672677683e71cc3ec85b02a33f3601d1a
Chelsea-Fox <chelsea@foxtrt.com>
2023-06-16 10:57:18 +0100

    main.py: Add task schema validation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 """
 Main file for TaskMaster
 """
+from datetime import datetime
+from schema import Schema
+from enum import Enum
+
+
+class Status(Enum):
+    OPEN = "OPEN"
+    DONE = "DONE"
+    CANCELLED = "CANCELLED"
 
 
 def get_task(task):
@@ -9,4 +18,12 @@ def get_task(task):
     :param task: str: The task to be accepted.
     :return: Task being passed
     """
+    schema = Schema({'description': str,
+                     'eta': datetime,
+                     'status': str})
+
+    Status(task["status"])
+
+    schema.validate(task)
+
     return task
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,25 @@ Main test file for TaskMaster
 """
 from unittest import TestCase
 from main import get_task
+from datetime import datetime, timedelta
 
 
 # pylint: disable=missing-class-docstring
 class Test(TestCase):
+    clean_task = {
+            "description": "Clean House",
+            "eta": datetime.now() + timedelta(days=3),
+            "status": "OPEN"
+        }
+
     def test_input_is_passed_and_returned(self):
         """Simple input and output"""
-        expected_value = "Clean"
-        actual_value = get_task("Clean")
+        expected_value = self.clean_task
+        actual_value = get_task(self.clean_task)
 
         self.assertEqual(expected_value, actual_value, "Values are not matching")
+
+    def test_valid_task_is_accepted(self):
+        return_value = get_task(self.clean_task)
+
+        assert return_value is not None
