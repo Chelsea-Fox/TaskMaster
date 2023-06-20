d0da0e9c60b03461124da98d8e42427350a3b79f
Chelsea-Fox <chelsea@foxtrt.com>
2023-06-20 15:02:59 +0100

    Create Flask app
    - Created Flask app with the following routes
      - GET /tasks
      - POST /task
      - GET /task/<id>
    - Added Test script that calls all routes

---
+++ src/app.py
@@ -0,0 +1,75 @@
+"""Flask app for task_master"""
+import json
+from datetime import datetime
+from functools import wraps
+
+from flask import Flask, request, make_response
+from tasks import Tasks
+
+app = Flask(__name__)
+tasks = Tasks()
+
+
+def format_response(func):
+    """Decorator to format response to json"""
+
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        # Invoke the original route function
+        response = func(*args, **kwargs)
+
+        # Convert the response to JSON
+        json_response = json.dumps(response)
+
+        # Create a Flask response with JSON content type
+        flask_response = make_response(json_response)
+        flask_response.headers["Content-Type"] = "application/json"
+
+        return flask_response
+
+    return decorated_function
+
+
+@app.route("/tasks")
+@format_response
+def tasks_get():
+    """Route /tasks"""
+    response = tasks.get_tasks()
+    for task in response:
+        convert_datetime_to_iso(task)
+
+    return response
+
+
+@app.route("/task", methods=["POST"])
+@format_response
+def tasks_post():
+    """Route for POST /task"""
+    task = request.get_json()
+    task["eta"] = datetime.strptime(task["eta"], "%Y-%m-%dT%H:%M:%S")
+
+    saved_task = tasks.post_task(task)
+
+    convert_datetime_to_iso(saved_task)
+
+    return saved_task
+
+
+@app.route("/task/<task_id>")
+@format_response
+def get_task(task_id):
+    """Route for GET /task>"""
+    response = tasks.get_tasks(task_id)
+
+    convert_datetime_to_iso(response[0])
+
+    return response
+
+
+def convert_datetime_to_iso(response):
+    """datetime to ISO string conversion"""
+    response["eta"] = response["eta"].isoformat()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
